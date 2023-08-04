6094b99bce0886acb398992a40b097c9e7c1346d
LuCancela <cancelalucas@gmail.com>
2023-08-03 22:35:16 -0300

    Laravel

---
+++ laravel/camargo/resources/views/inicial.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+    
+    <h1>Bom aqui vamos colocar algumas imagens</h1>
+
+    <img src="img/download.png">
+    <img src="img/pedro-atacante-flamengo-328x219.jpg">
+
+</body>
+</html>
